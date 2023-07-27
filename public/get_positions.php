f6ffd9eb19f557a5024adf03b9e2706a22ab9974
Timothy H <huynh.tk80@gmail.com>
2023-07-27 06:09:47 -0600

    feat: get positions from sql

---
+++ public/get_positions.php
@@ -0,0 +1,27 @@
+<?php
+
+// Include the database connection file
+require_once("../private/mysql_connection.php");
+
+// Get the selected staff ID from AJAX POST request
+$selectedStaffId = $_POST['staff_id'];
+
+// Query to fetch positions associated with the selected staff
+$sql = "SELECT position_id, position_name, hourly_rate, overtime_rate FROM position WHERE staff_id = " . $selectedStaffId;
+$result = $conn->query($sql);
+
+// Prepare the data for JSON response
+$positions = array();
+if ($result->num_rows > 0) {
+while ($row = $result->fetch_assoc()) {
+$positions[] = $row;
+}
+}
+
+// Close the database connection
+$conn->close();
+
+// Return positions as a JSON response
+header('Content-Type: application/json');
+echo json_encode($positions);
+?>
